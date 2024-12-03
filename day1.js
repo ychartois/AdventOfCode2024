7c19a200de934374933a106af10d201f5762870c
ychartois <ychartois@gmail.com>
2024-12-03 18:11:58 +0000

    day1 part2

--- day1.js
+++ day1.js
@@ -1,8 +1,20 @@
-function day1() {
+function day1_part1() {
   const sheet = SpreadsheetApp.getActive().getSheetByName("day1")
   const listA = sheet.getRange("A:A").getValues();
   const listB = sheet.getRange("B:B").getValues();
   const difference = listA.sort().map((num, idx) => num - listB.sort()[idx]);
   const distance = difference.reduce((a,b) => Math.abs(a) + Math.abs(b), 0);
   console.log(distance);
+}
+
+function day1_part2() {
+  const sheet = SpreadsheetApp.getActive().getSheetByName("day1")
+  const lastRow = sheet.getLastRow();
+  const listA = sheet.getRange(1,1,lastRow).getValues().map(row => row[0]);
+  const listB = sheet.getRange(1,2,lastRow).getValues().map(row => row[0]);
+
+  let sim = 0;
+  listA.forEach( valA => sim += listB.filter(valB => valA == valB).length * valA)
+
+  console.log(sim);
 }
